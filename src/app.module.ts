6175c09b0c1980d34e48ba84126d600f94d5fd27
Mateus Fernando <84980469+MateusFP95@users.noreply.github.com>
2022-06-23 14:47:43 -0300

    feat: create matrice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SouthtubModule } from './southtub/southtub.module';
 
 @Module({
-  imports: [],
+  imports: [SouthtubModule],
   controllers: [AppController],
   providers: [AppService],
 })
